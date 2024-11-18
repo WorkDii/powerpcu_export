5f9c64cb3cab15cfe539746d082fcb5b1f66b790
usman kareemee <usman2535@gmail.com>
2024-11-18 16:02:48 +0700

    update

--- export/controller/uploadData/update.ts
+++ export/controller/uploadData/update.ts
@@ -1,6 +1,8 @@
+import { directusClient } from "../../../lib/api.ts";
 import { mysqlClient } from "../../../lib/db.ts";
 import { QueryMap } from "../../../lib/type.ts";
 import { getTableName } from "../getTableName.ts";
+import { updateItem as directusUpdateItem } from "@directus/sdk";
 
 export const listUpdateItem = async (
   tableNames: ReturnType<typeof getTableName>
@@ -11,7 +13,7 @@ export const listUpdateItem = async (
       SELECT d.*
       FROM ${tableNames.data} d
       LEFT JOIN ${tableNames.sync} s ON d.PRIMARY_KEY_HASH = s.PRIMARY_KEY_HASH
-      WHERE s.ROW_HASH != d.ROW_HASH
+      WHERE s.ROW_HASH != d.ROW_HASH 
     `);
 
     return rows;
@@ -28,11 +30,15 @@ const updateItem = async (
     PRIMARY_KEY_HASH,
     ...item
   }: { ROW_HASH: string; PRIMARY_KEY_HASH: string },
-  sync_table: string
+  sync_table: string,
+  queryMap: QueryMap
 ) => {
-  // call rest
-  console.log(item);
-
+  await directusClient.request(
+    directusUpdateItem(queryMap.target_table, PRIMARY_KEY_HASH, {
+      ROW_HASH,
+      ...item,
+    })
+  );
   const conn = await mysqlClient.getConnection();
   try {
     const [rows] = await conn.query(
@@ -42,7 +48,6 @@ const updateItem = async (
     `,
       [ROW_HASH, PRIMARY_KEY_HASH]
     );
-
     return rows;
   } catch (error) {
     throw error;
@@ -54,8 +59,14 @@ const updateItem = async (
 export const uploadUpdateItems = async (queryMap: QueryMap) => {
   const tableNames = getTableName(queryMap);
   const updateItems = (await listUpdateItem(tableNames)) as any[];
-  for (const item of updateItems) {
-    await updateItem(item, tableNames.sync);
+  for (let index = 0; index < updateItems.length; index++) {
+    const item = updateItems[index];
+    console.log(
+      `update item ${item.PRIMARY_KEY_HASH} to directus ${index + 1} / ${
+        updateItems.length
+      }`
+    );
+    await updateItem(item, tableNames.sync, queryMap);
   }
   console.log("finish uploadUpdateItems");
 };
